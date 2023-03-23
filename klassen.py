3bd027b56136223711714a8bb85d43bb8c806ed6
CedricBSBT1 <128688618+CedricBSBT1@users.noreply.github.com>
2023-03-23 10:47:52 +0100

    Alle Klassen

--- klassen.py
+++ klassen.py
@@ -1,4 +1,26 @@
-class teacher:
-    def __init__(self):
-        self.first_name = ""
-        self.last_name = ""
+class Teacher:
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
+
+class Students:
+    def __init__(self, first_name: str, last_name: str):
+        first_name = first_name
+        last_name = last_name
+
+class Course:
+    def __init__(self, name: str):
+        self.name = name
+
+class Klasse:
+    def __init__(self, name: str, Students: list[Student] = []):
+        self.name = name
+        self.student = students
+
+class Grade:
+    def __init__(self, level: int, student: Students, weight: float, course: Course, date: date):
+        self.level = level
+        self.student = student
+        self.course = course
+        self.weight = weight
+        self.date = date
