630eefcbcd3ec2d2229521fe39492ae5caf831e5
michael.doskoczynski <mi.dos@gmx.de>
2023-03-23 11:09:15 +0100

    Code von Nachgruppe mit eingebunden datetime.date.today() funktioniert am besten

--- klassen.py
+++ klassen.py
@@ -1,26 +1,31 @@
-from datetime import date as Date
+import datetime
+
 
 class Teacher:
     def __init__(self, first_name: str, last_name: str):
         self.first_name = first_name
         self.last_name = last_name
 
+
 class Student:
     def __init__(self, first_name: str, last_name: str):
         first_name = first_name
         last_name = last_name
 
+
 class Course:
     def __init__(self, name: str):
         self.name = name
+
 
 class Klasse:
     def __init__(self, name: str, students: list[Student] = []):
         self.name = name
         self.students = students
 
+
 class Grade:
-    def __init__(self, level: int, student: Student, weight: float, course: Course, date: Date):
+    def __init__(self, level: int, student: Student, weight: float, course: Course, date):
         self.level = level
         self.student = student
         self.course = course
@@ -28,4 +33,6 @@ class Grade:
         self.date = date
 
 
-##Kommt jetzt eine Änderung wenn ich das hier schreibe?
+lukas = Student("Lukas", "Lang")
+biologie = Course("Biologie")
+mark = Grade(5, lukas, biologie, 2, datetime.date.today())
